1719d23c8456c156ee6572e2c24bf36518b3326d
Justin Barrick <justin.m.barrick@gmail.com>
2019-03-31 18:33:56 -0700

    factor clustering code out

--- main.go
+++ main.go
@@ -6,11 +6,8 @@ import (
 	"log"
 	"os"
 	"time"
-	hserf "github.com/hashicorp/serf/serf"
-	"github.com/justinbarrick/zeroconf-k8s/pkg/lock"
-	"github.com/justinbarrick/zeroconf-k8s/pkg/raft"
+	"github.com/justinbarrick/zeroconf-k8s/pkg/cluster"
 	"github.com/justinbarrick/zeroconf-k8s/pkg/kubeadm"
-	"github.com/justinbarrick/zeroconf-k8s/pkg/serf"
 )
 
 func main() {
@@ -24,76 +21,18 @@ func main() {
 
 	nodeName := os.Args[1]
 
-	serfPort := port
-	raftPort := port + 1
-	dsyncPort := port + 2
+	cluster := cluster.NewCluster(nodeName, "10.0.0.155", int(port), numInitialNodes)
 
-	rpcAddr := fmt.Sprintf("10.0.0.155:%d", dsyncPort)
-
-	leaderLock := lock.NewLock(rpcAddr, numInitialNodes)
-	leaderLock.AddNode(lock.NewClient(rpcAddr))
-
-	raft, err := raft.NewRaft(nodeName, "10.0.0.155", int(raftPort))
-	if err != nil {
+	if err = cluster.Start(os.Args[3:]); err != nil {
 		log.Fatal(err)
 	}
-
-	serf := serf.NewSerf(nodeName, int(serfPort))
-	serf.JoinCallback = func(event hserf.MemberEvent) {
-		if ! raft.Bootstrapped() {
-			for _, member := range serf.Members() {
-				memberRpcAddr := fmt.Sprintf("%s:%d", member.Addr.String(), member.Port + 2)
-				leaderLock.AddNode(lock.NewClient(memberRpcAddr))
-			}
-
-			lockAcquired, err := leaderLock.Lock()
-			if err != nil && err.Error() == "not enough nodes" {
-				return
-			} else if err != nil {
-				log.Fatal(err)
-			}
-
-			if lockAcquired {
-				if err = raft.Bootstrap(); err != nil {
-					log.Fatal("could not bootstrap raft", err)
-				}
-
-				if ! raft.Leader() {
-					return
-				}
-
-				if err := raft.Apply([]byte("hello world")); err != nil {
-					log.Fatal("error writing to raft", err)
-				}
-			}
-		}
-
-		if raft.Bootstrapped() && raft.Leader() {
-			for _, member := range serf.Members() {
-				if member.Name == nodeName {
-					continue
-				}
-
-				if err := raft.AddNode(member.Name, member.Addr, member.Port + 1); err != nil {
-					log.Fatal("error adding member", err)
-				}
-
-				fmt.Println("ADDED MEMBER", member.Addr, member.Port, member.Name)
-			}
-		}
-	}
-
-	if err := serf.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	serf.Join(os.Args[3:])
 
 	k := kubeadm.Kubeadm{
 		APIServer: "k8s.example.com",
 		Token: "abcdef.abcdef12abcdef12",
 		CertificateKey: "abcd",
 	}
+
 	fmt.Println(k.InitWorker())
 
 	time.Sleep(60 * time.Second)
---
+++ pkg/cluster/cluster.go
@@ -0,0 +1,101 @@
+package cluster
+
+import (
+	"fmt"
+	"log"
+	hserf "github.com/hashicorp/serf/serf"
+	"github.com/justinbarrick/zeroconf-k8s/pkg/lock"
+	"github.com/justinbarrick/zeroconf-k8s/pkg/raft"
+	"github.com/justinbarrick/zeroconf-k8s/pkg/serf"
+)
+
+type Cluster struct {
+	port int
+	addr string
+	nodeName string
+	numInitialNodes int
+	raft *raft.Raft
+	serf *serf.Serf
+	lock *lock.Lock
+}
+
+func NewCluster(nodeName, addr string, port int, numInitialNodes int) *Cluster {
+	return &Cluster{
+		nodeName: nodeName,
+		port: port,
+		addr: addr,
+		numInitialNodes: numInitialNodes,
+	}
+}
+
+func (c *Cluster) Start(bootstrapAddrs []string) error {
+	var err error
+
+	serfPort := c.port
+	raftPort := c.port + 1
+	dsyncPort := c.port + 2
+
+	c.raft, err = raft.NewRaft(c.nodeName, c.addr, int(raftPort))
+	if err != nil {
+		return err
+	}
+
+	c.serf = serf.NewSerf(c.nodeName, int(serfPort))
+	c.serf.JoinCallback = c.JoinCallback
+
+	rpcAddr := fmt.Sprintf("%s:%d", c.addr, dsyncPort)
+
+	c.lock = lock.NewLock(rpcAddr, c.numInitialNodes)
+	c.lock.AddNode(lock.NewClient(rpcAddr))
+
+	if err := c.serf.Start(); err != nil {
+		return err
+	}
+
+	c.serf.Join(bootstrapAddrs)
+	return nil
+}
+
+func (c *Cluster) JoinCallback(event hserf.MemberEvent) {
+	if ! c.raft.Bootstrapped() {
+		for _, member := range c.serf.Members() {
+			memberRpcAddr := fmt.Sprintf("%s:%d", member.Addr.String(), member.Port + 2)
+			c.lock.AddNode(lock.NewClient(memberRpcAddr))
+		}
+
+		lockAcquired, err := c.lock.Lock()
+		if err != nil && err.Error() == "not enough nodes" {
+			return
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		if lockAcquired {
+			if err = c.raft.Bootstrap(); err != nil {
+				log.Fatal("could not bootstrap raft", err)
+			}
+
+			if ! c.raft.Leader() {
+				return
+			}
+
+			if err := c.raft.Apply([]byte("hello world")); err != nil {
+				log.Fatal("error writing to raft", err)
+			}
+		}
+	}
+
+	if c.raft.Bootstrapped() && c.raft.Leader() {
+		for _, member := range c.serf.Members() {
+			if member.Name == c.nodeName {
+				continue
+			}
+
+			if err := c.raft.AddNode(member.Name, member.Addr, member.Port + 1); err != nil {
+				log.Fatal("error adding member", err)
+			}
+
+			fmt.Println("ADDED MEMBER", member.Addr, member.Port, member.Name)
+		}
+	}
+}
